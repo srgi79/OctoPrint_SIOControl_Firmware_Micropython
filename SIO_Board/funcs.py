da102e8918185cb8091c009603fa556c6521e345
srgi79 <36154007+srgi79@users.noreply.github.com>
2024-10-29 20:08:14 +0100

    Add files via upload

--- SIO_Board/funcs.py
+++ SIO_Board/funcs.py
@@ -1,14 +1,26 @@
 
-def set_led(state: bool):
-    from machine import Pin
-    
-    led = Pin(25, Pin.OUT)
+# GLOBAL UART1
+from machine import UART, Pin
+uart1 = UART(1, baudrate=115200, tx=Pin(4), rx=Pin(5), timeout=250)
+
+# GLOBAL RESERVED GPIOS https://pico.pinout.xyz/
+reserved_gpios = [0, 1, 4, 5, 23, 24, 29]
+# GP00	UART0 TX
+# GP01	UART0 RX
+# GP04	UART1 TX
+# GP05	UART1 RX
+# GP23	RT6150B-33GQW Power-Select
+# GP24	VBUS Sense
+# GP25	User LED
+# GP29 / A3	VSYS Sense
+
+def set_led(state: bool):    
+    led = Pin("LED", Pin.OUT)
     led.value(state)
     print(f"DEBUG: Led set to {state}")
 
 
 def set_rgb(tup: tuple):
-    from machine import Pin
     from neopixel import NeoPixel
     
     np = NeoPixel(Pin(23), 1)
@@ -17,16 +29,22 @@ def set_rgb(tup: tuple):
 
 
 def read_uart():
-    from machine import UART, Pin
+    global uart1
     
-    uart1 = UART(1, baudrate=115200, tx=Pin(4), rx=Pin(5), timeout=250)
-
+    # Read uart stream
     b = uart1.readline()
     
+    # Check stream
     if b != b'' and b is not None:
-        s = b.decode('utf-8')
-        #print(f"READ {s}")
-        return s
+        try:
+            s = b.decode('utf-8')
+            if s.endswith('\n'):
+                print(f"READ {s[:-1]}")
+            else:
+                print(f"READ {s}")
+            return s
+        except:
+            return ""
     else:
         return f""
 
@@ -38,9 +56,9 @@ def switch_cmd(cmd):
     elif cmd.startswith("VC"): #Returns the version and compatibility messaging. This is used by the Octoprint_SIOPlugin to determine if it is a compatible device
         return ["VC", "", ""]
     elif cmd.startswith("IC"): #Returns the number of IO points being monitored
-        return ["IC", "", ""]
-    elif cmd.startswith("CIO"): #Ex: CIO 1,1,1,1,5,5,5,5,5,5,5,2,2,2
-        return ["CIO", "1,1,1,1,5,5,5,5,5,5,5,2,2,2", ""]
+        return ["IC", "28", ""]
+    elif cmd.startswith("CIO"): #Ex: CIO 1,1,1,1,5,5,5,5,5,5,5,2,2,2. 1:IN, 2:OUT, 5:IN_PULLUP, 9:IN_PULLDOWN, 18:OUT_OPEN_DRAIN, -2:OUT_PWN, -3:IN_DHT
+        return ["CIO", "2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,", ""]
     elif cmd.startswith("SIO"): #Stores current IO point type settings to local storage
         return ["SIO", "", ""]
     elif cmd.startswith("IO"): #Sets an IO point. Ex: IO [#] [0/1]
@@ -57,25 +75,60 @@ def switch_cmd(cmd):
         return ["GS", "", ""]
     elif cmd.startswith("N") or (cmd == "M115"):
         return ["N", "", ""]
-    elif cmd.startswith("RR"): 
-        return ["RR", "", ""]
-    elif cmd.startswith("DG"): 
-        return ["DG", "", ""]
     elif cmd.startswith("restart") or cmd.startswith("reboot") or cmd.startswith("reset"): #Forces a restart of the device
         return ["restart", "", ""]
     else:
         return ["", "", ""]
 
 def send_ack():
-    from machine import UART, Pin
-    
-    uart1 = UART(1, baudrate=115200, tx=Pin(4), rx=Pin(5), timeout=250)
+    global uart1    
 
     uart1.write("OK\n")
 
 def write_uart(msg):
-    from machine import UART, Pin
-    
-    uart1 = UART(1, baudrate=115200, tx=Pin(4), rx=Pin(5), timeout=250)
+    global uart1        
 
-    uart1.write(msg)
+    uart1.write(msg+"\n")
+
+def create_report():
+    from machine import mem32
+    global reserved_gpios
+    
+    
+    max_gpio = 29 #GP0 to GP29
+    #GPIO0 to GPIO22 are digital only
+    #GPIO 26-28 are able to be used either as digital GPIO or as ADC inputs
+    
+    msg = "IO:"
+    addrGP0Status= 0x40014000
+    
+    for g in (range(max_gpio+1)): #Not reversed
+        if not g in reserved_gpios:
+            addrGPXStatus = g*0x08 + 0x40014000
+            reg = mem32[addrGPXStatus]
+            val = (reg >> 9) & 0x1
+            msg += f"{val}"
+        else:
+            msg += "0"
+    
+    return msg
+
+def store_settings():
+    print("TODO")
+
+def set_io(pin_n: int, pin_state: bool):
+    #from machine import Pin
+    global reserved_gpios
+    
+    if pin_n in reserved_gpios:
+        print(f"DEBUG: Pin {pin_n} is reserved")
+    else:
+        pin = Pin(pin_n, Pin.OUT)
+        pin.value(pin_state)
+        print(f"DEBUG: Pin {pin_n} set to {pin_state}")
+
+def restart_board():
+    from machine import soft_reset
+    
+    print(f"DEBUG: Restarting")
+    soft_reset()
--- SIO_Board/main.py
+++ SIO_Board/main.py
@@ -1,9 +1,9 @@
 
-from funcs import set_led, set_rgb, read_uart, switch_cmd, send_ack, write_uart
+from funcs import set_led, set_rgb, set_io, read_uart, write_uart, switch_cmd, send_ack, create_report, store_settings, restart_board
 from time import sleep, ticks_ms, ticks_diff
 
 # Define allowed cmds
-allowed_cmds = ["EIO", "BIO", "VC", "IC", "CIO", "SIO", "IO", "IOT", "SI", "SE", "GS", "N", "RR", "DG", "restart"]
+allowed_cmds = ["EIO", "BIO", "VC", "IC", "CIO", "SIO", "IO", "IOT", "SI", "SE", "GS", "N", "restart"]
 
 # Initilize variables
 report_enable = True
@@ -19,6 +19,12 @@ set_rgb((0,0,0))
 # Initilize reporting timer
 now = ticks_ms()
 
+# Send RR IO ready for commands
+write_uart(f"RR")
+
+# Send first report
+cmdReport = True
+
 while 1:
     
     # Read and decode ByteStream to receive
@@ -27,82 +33,81 @@ while 1:
         # Interpret received command
         c = switch_cmd(r)
         
-        # ACK
+        # ACK and Execute command
         if c[0] in allowed_cmds:
+            # Send ACK
             send_ack()
 
-        # Execute command
-        if c[0] == "EIO": #Pause/End IO Status Auto reporting. This setting is not maintained through restarts of the device
-            report_enable = False
-            print(f"DEBUG: Report disabled")
-            
-        elif c[0] == "BIO": #Begin AutoReporting IO status (only needed if EIO has been called)
-            report_enable = True
-            print(f"DEBUG: Report enabled")
-            
-        elif c[0] == "VC": #Returns the version and compatibility messaging. This is used by the Octoprint_SIOPlugin to determine if it is a compatible device
-            write_uart(f"VI:SIO_Arduino_General 1.0.11\nCP:SIOPlugin 0.1.1\nFS:NA\nXT BRD:ATmega328P")
-            print(f"DEBUG: Sending VC")
-            
-        elif c[0] == "IC": #Returns the number of IO points being monitored
-            write_uart(f"IC:17")
-            print(f"DEBUG: Sending IC")
-            
-        elif c[0] == "CIO": #Ex: CIO 1,1,1,1,5,5,5,5,5,5,5,2,2,2
-            write_uart(f"{c[0]} {c[1]}")
-            print(f"DEBUG: Sending CIO")
-            
-        elif c[0] == "SIO": #Stores current IO point type settings to local storage
-            #store_settings()
-            print(f"DEBUG: Settings Stored")
-            
-        elif c[0] == "IO": #Sets an IO point. Ex: IO [#] [0/1]
-            set_led(bool(int(c[2])))
-            print(f"DEBUG: IO {c[1]} set to {c[2]}")
-            
-        elif c[0] == "IOT": #Outputs the current IO Point types pattern as a single string of integers
-            print(f"DEBUG: IOT")
-            
-        elif c[0] == "SI": #Sets the auto report interval
-            report_interval = int(c[1])
-            print(f"DEBUG: Report interval set to {c[1]}ms")
-            
-        elif c[0] == "SE": #Enables or disables event triggering of IO Reports
-            print(f"DEBUG: SE")
-            
-        elif c[0] == "GS": #Will force an IO status report
-            #report = create_report()
-            report = "IO 000000000000000000" #18 TEST
-            print(f"REPORT: {report}")
-            #send_report(report)
-            now = ticks_ms()
-            
-        elif c[0] == "N": #NOT a printer
-            print(f"DEBUG: N")
-            
-        elif c[0] == "RR": #Restart?
-            print(f"DEBUG: RR")
-            
-        elif c[0] == "DG": #Debug?
-            print(f"DEBUG: DG")
-            
-        elif c[0] == "restart": #Restart
-            print(f"DEBUG: Restarting")
-            
-        else:
-            print(f"DEBUG: CMD UNKNOWN")
+            # Execute command
+            if c[0] == "EIO": #Pause/End IO Status Auto reporting. This setting is not maintained through restarts of the device
+                report_enable = False
+                print(f"DEBUG: Report disabled")
+                
+            elif c[0] == "BIO": #Begin AutoReporting IO status (only needed if EIO has been called)
+                report_enable = True
+                print(f"DEBUG: Report enabled")
+                
+            elif c[0] == "VC": #Returns the version and compatibility messaging. This is used by the Octoprint_SIOPlugin to determine if it is a compatible device
+                write_uart(f"VI:SIO_ESP32WRM_Relay_X2 1.1.4\nCP:SIOPlugin 0.1.1\nFS:16MB\nXT BRD:RP2040")
+                print(f"DEBUG: Sending VC")
+                
+            elif c[0] == "IC": #Returns the number of IO points being monitored
+                write_uart(f"IC:28")
+                print(f"DEBUG: Sending IC")
+                
+            elif c[0] == "CIO": #Ex: CIO 1,1,1,1,5,5,5,5,5,5,5,2,2,2
+                write_uart(f"{c[0]} {c[1]}")
+                print(f"DEBUG: Sending CIO")
+                
+            elif c[0] == "SIO": #Stores current IO point type settings to local storage
+                #store_settings()
+                print(f"DEBUG: Settings Stored")
+                
+            elif c[0] == "IO": #Sets an IO point. Ex: IO [#] [0/1]
+                set_io(int(c[1]), bool(int(c[2])))
+                # Send report
+                cmdReport = True
+                print(f"DEBUG: IO {c[1]} set to {c[2]}")
+                
+            elif c[0] == "IOT": #Outputs the current IO Point types pattern as a single string of integers
+                write_uart(f"IT:2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,")
+                print(f"DEBUG: IOT")
+                
+            elif c[0] == "SI": #Sets the auto report interval
+                report_interval = int(c[1])
+                print(f"DEBUG: Report interval set to {c[1]} ms")
+                
+            elif c[0] == "SE": #Enables or disables event triggering of IO Reports
+                print(f"DEBUG: SE")
+                
+            elif c[0] == "GS": #Will force an IO status report
+                report = create_report()
+                print(f"REPORT: {report}")
+                send_report(report)
+                # Reset timer
+                now = ticks_ms()
+                
+            elif c[0] == "N": #NOT a printer
+                write_uart("//action:disconnect")
+                print(f"DEBUG: N")
+                
+            elif c[0] == "restart": #Restart
+                restart_board()
+                
+            else:
+                print(f"DEBUG: CMD UNKNOWN")
 
 
     # Reporting
-    if ticks_diff(ticks_ms(), now) > report_interval:
+    if cmdReport or (ticks_diff(ticks_ms(), now) > report_interval):
+        cmdReport = False
         if report_enable:
-            #report = create_report()
-            report = "IO 000000000000000000" #18 TEST
+            report = create_report()
             print(f"REPORT: {report}")
-            #send_report(report)
+            write_uart(report)
         now = ticks_ms()
-
-
+    elif ticks_diff(ticks_ms(), now) > 60*1000:
+        write_uart(f"RR")
 
 
 
